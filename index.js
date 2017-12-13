8e825670580396c22b0958f6362f91c73c514b50
dvitvitskiy.pro <dvitvitskiy.pro@gmail.com>
2017-12-13 23:30:44 +0300

    add routes

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ application.use(cors()) ;
 
 application.get('/', function(req, res){
   // res.send(result);
+  console.log(`[SERVER] => posts`);
   res.json(posts);
 });
 
 application.get('/post/:id', function(req, res){
+  console.log(`[SERVER] => post #${req.params.id-1}`);
   res.json(posts[req.params.id-1]);
+});
+
+application.post('/post/:id/like', function(req, res){
+  posts[req.params.id-1].like += 1;
+  console.log(`[SERVER] => post #${req.params.id-1} => ${posts[req.params.id-1].like} likes`);
+  res.json(posts[req.params.id-1].likes);
+});
+
+application.post('/post/:id/dislike', function(req, res){
+  posts[req.params.id-1].dislike += 1;
+  console.log(`[SERVER] => post #${req.params.id-1} => ${posts[req.params.id-1].dislike} dislikes`);
+  res.json(posts[req.params.id-1].dislikes);
 });
 
 application.listen(3001, function() {
